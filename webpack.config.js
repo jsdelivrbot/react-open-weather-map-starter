de7ea40a123e1e2631a1989bcd9fc5507cac6400
Hemã Vidal <hemavidal@gmail.com>
2018-03-19 00:12:24 -0300

    Initial Webpack configuration finished.
    Hot reload is working fine with react.

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = env => {
+  if (env && env.NODE_ENV) {
+    console.log('Environment: ', env.NODE_ENV)
+  }
+
+  return {
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    entry: './src/containers/AppContainer.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+      filename: 'app.bundle.js'
+    },
+    module: {
+      rules: [
+        { test: /\.css$/,
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" }
+          ]
+        },
+        {
+          test: /\.js|\.jsx?$/,
+          exclude: /node_modules/,
+          use: "babel-loader"
+        }
+      ]
+    }
+  }
+};
